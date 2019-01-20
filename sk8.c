a5f0ab73a74aa22c4d7f191a63a25222dcc81e9b
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-01-20 06:15:21 -0800

    Create sk8.c

---
+++ sk8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0;
+clrscr();
+printf("Enter the n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+getch();
+}

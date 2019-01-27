459f03b9abce42118d3d13f2021b5139e2ff8863
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-01-27 04:52:28 -0800

    Create sk14.c

---
+++ sk14.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i;
+    printf("Enter a and b");
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+     if(i%2!=0)
+     {
+         printf("%d",i);
+         printf(" ");
+         }
+     }
+    
+    return 0;
+}

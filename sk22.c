48510cd7e4fb1d258366d68084f63be9c310e067
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-01-27 06:37:43 -0800

    Create sk22.c

---
+++ sk22.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,max,i;
+    int b[10];
+    printf("Enter n");
+    scanf("%d",&n);
+    printf("Enter array");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    max=b[0];
+    for(i=0;i<n;i++)
+    {
+    if(max<b[i])
+    {
+    max=b[i];
+    }}
+    printf("%d",max);
+    return 0;
+}

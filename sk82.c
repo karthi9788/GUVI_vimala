8656e53fef234bcd9be5e7d34a64ee8e90c9435d
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-01-28 08:58:36 -0800

    Create sk82.c

---
+++ sk82.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c;
+    printf("Enter the length of farm");
+    scanf("%f",&a);
+    printf("Enter the breadth of frame");
+    scanf("%f",&b);
+    c=a*b;
+    printf("%.5f",c);
+
+    return 0;
+}

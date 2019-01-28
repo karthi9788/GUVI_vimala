8c417d42c3ac6b2256edcc0403f869a46fae7d76
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-01-28 08:54:30 -0800

    Create sk81.c

---
+++ sk81.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter the no of ninjas in kabalis clan");
+    scanf("%d",&a);
+    printf("Enter the no of ninjas in opposites clan");
+    scanf("%d",&b);
+    c=b-a;
+    printf("%d",c);
+
+    return 0;
+}

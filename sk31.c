719e3f4e4db084f3b0aa78739562fc8c1341c2b3
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-06 06:28:11 -0800

    Rename sk21.c to sk31.c

---
+++ sk31.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    
+    char a[20];
+    int i,n;
+    printf("Enter no of times");
+    scanf("%d",&n);
+   printf("Enter string");
+   scanf("%s",a);
+   for(i=0;i<n;i++)
+   {
+   printf("%s\n",a);
+   }
+    
+    
+    return 0;
+}

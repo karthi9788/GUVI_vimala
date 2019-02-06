f750ea613273ca51d25ce06a774efe4e3b87c449
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-06 06:13:47 -0800

    Create sk50.c

---
+++ sk50.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    
+    int n,a=0,b=1,i,sum=0;
+    printf("enter the range");
+    
+        scanf("%d",&n);
+    
+    
+    
+    for(i=0;i<n;i++)
+    {
+        printf("%d",b);
+    sum=a+b;
+     a=b;
+    b=sum;
+   
+    
+    }
+
+    return 0;
+}

bacdb0c2081e0b0b3eca148477e4c7decfa5e370
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-06 05:21:21 -0800

    Rename sk4.c to sk44.c

---
+++ sk44.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[20];
+    int i,count=0;
+    printf("enter the string");
+    scanf("%[^\n]",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+    if(a[i]=='.')
+    {
+        count++;
+    }
+    
+     
+    }
+printf("%d",count);
+    return 0;
+}

e6c5f9b1c1ac3f4e08a6de6375f46e6a9e077741
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-07 02:42:16 -0800

    Create sk34.c

---
+++ sk34.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf(" enter value n");
+    
+    scanf("%d",&n);
+    if(n>=1&&n<=10)
+    {
+    printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return(0);
+}

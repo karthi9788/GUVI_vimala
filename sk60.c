5e122eefdec86b9480de8eedf7e7b6705ff98f96
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 04:08:20 -0800

    Create sk60.c

---
+++ sk60.c
@@ -0,0 +1,18 @@
+
+
+
+#include<stdio.h>
+#include<math.h>
+int  main()
+{
+
+  int a,i,sum=0;
+  printf("Enter a");
+  scanf("%d",&a);
+ for(i=1;i<=a;i++)
+ {
+     sum=sum+i;
+ }
+ printf("%d",sum);
+    return(0);
+}

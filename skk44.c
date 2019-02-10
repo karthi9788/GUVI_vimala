2336368750c09aba3399e9034899ff9bd367ae90
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-09 23:11:39 -0800

    Create skk44.c

---
+++ skk44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int  main()
+{
+    int a;
+    printf("Enter a");
+    scanf("%d",&a);
+    if(a>=1&&a<=10)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+ return (0);   
+}

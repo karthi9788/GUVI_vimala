b174087fb13f8a7e97fe009a9151d22939fd0906
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 05:01:14 -0800

    Create sk64.c

---
+++ sk64.c
@@ -0,0 +1,20 @@
+
+
+
+
+#include<stdio.h>
+void  main()
+{
+int a,b,c;
+printf("Enter a and b");
+scanf("%d%d",&a,&b);
+c=a+b;
+if(c%2!=0)
+{
+    printf("Yes");
+}
+else
+{
+    printf("No");
+}
+}

1bca5aa3e144c5d47ff3dd914c83ddab2bc60426
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 05:56:27 -0800

    Create sk95.c

---
+++ sk95.c
@@ -0,0 +1,19 @@
+
+
+
+
+#include<stdio.h>
+
+void  main()
+{
+int a,b,c,si;
+printf("Enter p");
+scanf("%d",&a);
+printf("Enter t");
+scanf("%d",&b);
+printf("Enter r");
+scanf("%d",&c);
+
+si=(a*b*c)/100;
+printf("%d",si);
+}

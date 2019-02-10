cb28d7b72756b1a4a9e660564aa223655f7257fa
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 03:53:45 -0800

    Create sk57.c

---
+++ sk57.c
@@ -0,0 +1,28 @@
+
+
+#include<stdio.h>
+#include<math.h>
+int  main()
+{
+    int a[20];
+    int i,n,b,count=0;
+    printf("Enter n");
+    scanf("%d",&n);
+    printf("Enter a");
+    scanf("%d",&b);
+   printf("Enter array");
+   for(i=0;i<n;i++)
+   {
+   scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   
+   {
+       if(a[i]==b)
+       {
+           count++;
+       }
+   }
+   printf("%d",count);
+    return(0);
+}

87b6933949143bf1d48b2f81cb1ee027f0fb447e
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 05:16:12 -0800

    Create sk68.c

---
+++ sk68.c
@@ -0,0 +1,22 @@
+
+
+
+
+#include<stdio.h>
+void  main()
+{
+int a,i;
+printf("Enter no");
+scanf("%d",&a);
+
+    if(a%7==0)
+    {
+        printf("Yes");
+        
+    }
+    else
+    {
+        printf("No");
+    }
+
+}

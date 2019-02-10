7bdd8a36fe8a5886aee2511bb8cf4adb18ad1dd9
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 06:01:28 -0800

    Create sk96.c

---
+++ sk96.c
@@ -0,0 +1,18 @@
+
+
+
+
+#include<stdio.h>
+
+void  main()
+{
+int a,d,n,sn;
+printf("Enter a");
+scanf("%d",&a);
+printf("Enter d");
+scanf("%d",&d);
+printf("Enter n");
+scanf("%d",&n);
+sn=(n/2)*(2*a+(n-1)*d);
+printf("%d",sn);
+}

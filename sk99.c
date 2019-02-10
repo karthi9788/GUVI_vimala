676227bf32e088ffb630b4ca6acd039793bb60c8
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-10 06:17:12 -0800

    Create sk99.c

---
+++ sk99.c
@@ -0,0 +1,21 @@
+
+
+
+
+#include<stdio.h>
+
+void  main()
+{
+int a,b,c,d,e;
+
+printf("Enter a");
+scanf("%d",&a);
+printf("Enter a");
+scanf("%d",&b);
+printf("Enter a");
+scanf("%d",&c);
+d=a*b;
+e=d%c;
+printf("%d",e);
+
+}

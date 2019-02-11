19715cb3f97cace31d3f63b9e86845f5e218220e
karthi9788 <30971708+karthi9788@users.noreply.github.com>
2019-02-11 15:54:59 +0530

    Create sk78.c

---
+++ sk78.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+ int a,i;
+ printf("Enter no");
+ scanf("%d",&a);
+ 
+     if(a%13==0)
+     {
+        printf("Yes");
+     }
+ else
+ {
+     printf("No");
+ }
+
+    return 0;
+}
